622f202a2d34617a03d546625e493e33329f7587
Njagalavibe <patricolumu@gmail.com>
2023-08-03 15:44:47 +0300

    working on javaScript

---
+++ index.js
@@ -0,0 +1,6 @@
+let addBook =document.querySelector(".add-book");
+
+addBook.addEventListener("click", function(){
+    let form = document.querySelector(".input-form")
+    form.style.display = "flex";
+})
