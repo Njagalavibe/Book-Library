36fee556c8a8598e9630d212deca8bd6bc26264f
Njagalavibe <patricolumu@gmail.com>
2023-08-08 19:42:56 +0300

    working on JS

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function render() {
                 <h3 class="book-author">Author : ${book.author}</h3>
                 <h4 class="book-pages">Pages : ${book.pages} Pages</h4>
                 <h4 class="book-language">Languages : ${book.language}</h4>
-                <h4 class="book-progress">Read : ${book.read ? 'Complete' : "Not Complete"}</h4>
+                <h4 class="book-progress">Read : ${book.read ? 'Complete' : 'Not Complete'}</h4>
                 </div>
                 <div class="buttons">
                     <button class="delete-button" onclick = "deleteBook(${i})">DELETE</button>
@@ -44,58 +44,58 @@ function render() {
             
         </div>`;
         addedBook.appendChild(bookDetails);
-        let bookProgress = document.querySelector(".book-progress")
-        
-            if (book.read == true) {
-                bookProgress.style.backgroundColor = "#16C79A";
-                
+
+        /* let bookProgress = document.querySelector(".book-progress")
+             if (book.read == true) {
+                 bookProgress.style.backgroundColor = "#16C79A";
+                 
+ 
+ 
+             } else {
+                 bookProgress.style.backgroundColor = "#FF6363";
+             }*/
+         
+         
+ 
+     }
+ }
+ 
+ function deleteBook(index) {
+     myLibrary.splice(index, 1);
+     render()
+ }
+ 
+ /*
+ let deleteButton = document.querySelector(".delete-button")
+ deleteButton.innerHTML = "<i class="fa-solid fa-bin-recycle"></i>"
+ */
+
+        function addBookToLibrary() {
+            let title = document.querySelector("#title").value;
+            let author = document.querySelector("#author").value;
+            let pages = document.querySelector("#pages").value;
+            let language = document.querySelector("#language").value;
+            let read = document.querySelector(".tick").checked;
+            let newBook = new Book(title, author, pages, language, read);
+            myLibrary.push(newBook);
+
+            render();
+        }
+
+        addBook.addEventListener("click", function () {
+            let form = document.querySelector(".input-form");
+            form.style.display = "flex";
+
+        })
+
+        form.addEventListener("submit", function (e) {
+            e.preventDefault();
+            addBookToLibrary();
+            form.style.display = "none";
+            form.reset();
 
 
-            } else {
-                bookProgress.style.backgroundColor = "#FF6363";
-            }
-        
-        
-
-    }
-}
-
-function deleteBook(index) {
-    myLibrary.splice(index, 1);
-    render()
-}
-
-/*
-let deleteButton = document.querySelector(".delete-button")
-deleteButton.innerHTML = "<i class="fa-solid fa-bin-recycle"></i>"
-*/
-
-function addBookToLibrary() {
-    let title = document.querySelector("#title").value;
-    let author = document.querySelector("#author").value;
-    let pages = document.querySelector("#pages").value;
-    let language = document.querySelector("#language").value;
-    let read = document.querySelector(".tick").checked;
-    let newBook = new Book(title, author, pages, language, read);
-    myLibrary.push(newBook);
-
-    render();
-}
-
-addBook.addEventListener("click", function () {
-    let form = document.querySelector(".input-form");
-    form.style.display = "flex";
-
-})
-
-form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    addBookToLibrary();
-    form.style.display = "none";
-    form.reset();
-
-
-})
+        })
 
 
 
