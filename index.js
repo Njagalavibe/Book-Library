d5d12c45dba531911d109ba023c11d10f2166c57
Njagalavibe <patricolumu@gmail.com>
2023-08-08 16:21:07 +0300

    working on js

--- index.js
+++ index.js
@@ -5,34 +5,47 @@ let form = document.querySelector(".input-form");
 
 let myLibrary = [];
 
-function Book(title,author,pages,language,read) {
-  this.title = title,
-  this.author = author,
-  this.pages = pages,
-  this.language = language
-  this.read = read;
+function Book(title, author, pages, language, read) {
+    this.title = title,
+        this.author = author,
+        this.pages = pages,
+        this.language = language
+    this.read = read;
 }
 
-function render(){
+function render() {
     let addedBook = document.querySelector("#added-books");
-    addedBook.innerHTML =""
-    for (let i =0; i < myLibrary.length; i++){
+    addedBook.innerHTML = ""
+
+    for (let i = 0; i < myLibrary.length; i++) {
         let book = myLibrary[i];
         let bookDetails = document.createElement("div");
         bookDetails.innerHTML = `
         <div class="book-card">
 
             <h3 class="book-title">Title : ${book.title}</h3>
-            <h3 class="book-author">Author : ${book.author}</h3>
-            <h4 class="book-pages">Pages : ${book.pages}</h4>
-            <h4 class="book-language">Languages : ${book.language}</h4>
-            <h4 class="book-progress">Read : ${book.read ? "Completed" : "Not Complete"}</h4>
-            
+            <div class="book-details">
+                <h3 class="book-author">Author : ${book.author}</h3>
+                <h4 class="book-pages">Pages : ${book.pages} Pages</h4>
+                <h4 class="book-language">Languages : ${book.language}</h4>
+                <h4 class="book-progress">Read : ${book.read ? 'Complete' : "Not Complete"}</h4>
+            </div>
         </div>`;
         addedBook.appendChild(bookDetails);
+        let bookProgress = document.querySelector(".book-progress")
+        if (book.read == true) {
+            bookProgress.style.backgroundColor = "green";
+            
+        } else {
+            bookProgress.style.backgroundColor = "red";
+           
+        }
+
     }
 
+
 }
+
 
 function addBookToLibrary() {
     let title = document.querySelector("#title").value;
@@ -40,19 +53,22 @@ function addBookToLibrary() {
     let pages = document.querySelector("#pages").value;
     let language = document.querySelector("#language").value;
     let read = document.querySelector(".tick").checked;
-    let newBook = new Book(title,author,pages,language,read);
+    let newBook = new Book(title, author, pages, language, read);
     myLibrary.push(newBook);
     render();
 }
 
-addBook.addEventListener("click", function(){
+addBook.addEventListener("click", function () {
     let form = document.querySelector(".input-form");
     form.style.display = "flex";
+
 })
 
-form.addEventListener("submit",function(e){
+form.addEventListener("submit", function (e) {
     e.preventDefault();
     addBookToLibrary();
-    
+    form.style.display = "none";
+    form.reset();
+
 
 })
